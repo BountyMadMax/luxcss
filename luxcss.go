913e2f7e97e4c4576ee8199050d44a09ad6410e7
Yannick Melzer <yannick@fam-melzer.de>
2024-07-25 22:21:39 +0200

    Remove profiling and rebuild

--- luxcss.go
+++ luxcss.go
@@ -1,34 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"luxcss/lib"
 	"os"
-	"runtime"
-	"runtime/pprof"
 
 	"github.com/urfave/cli/v2"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
-
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
 	var watch bool
 	var prefix string
 	var output string
@@ -90,17 +71,5 @@ func main() {
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
-	}
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close()
-		runtime.GC()
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
 	}
 }
